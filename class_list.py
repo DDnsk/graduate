631a6b9c712d270f39e6b97720c25f192e0ff20a
Dnsk <380712421@qq.com>
2017-04-19 17:31:10 +0800

    in the process of building tree from article

--- class_list.py
+++ class_list.py
@@ -1,33 +1,52 @@
 # coding=utf-8
 __author__ = 'Dnsk'
 
-class paragraph():
+class Article:
     """
 
     """
-    title_of_part = ''
-    part_index = 0
-    index_in_part = 0
+    def __init__(self, title):
+        self.title_main = title
+        self.part_list = []
 
-    # contains sentence instances that in this paragraph
-    sentence_containing = []
-
-class sentence():
+class Part:
     """
 
     """
-    index_in_paragraph = 0
-    original_sentence = ''
-    tf_isf = {}
+    def __init__(self):
+        self.title_part = ''
+        self.index_in_article = 0
+        self.paragraph_list = []
+
+
+class Paragraph:
+    """
+
+    """
+    def __init__(self):
+        self.index_in_part = 0
+        # contains sentence instances that in this paragraph
+        self.sentence_containing = []
+
+class Sentence:
+    """
+
+    """
+    def __init__(self):
+        self.index_in_paragraph = 0
+        self.original_sentence = ''
+        self.tf_isf = {}
+
     def sentence_length(self):
         return len(self.tf_isf)
 
+
 if __name__ == '__main__':
-    para_1 = paragraph()
+    para_1 = Paragraph()
     para_1.title_of_part = 'Introduction'
-    sen_1 = sentence()
+    sen_1 = Sentence()
     sen_1.original_sentence = 'Hello World!'
-    sen_2 = sentence()
+    sen_2 = Sentence()
     sen_2.original_sentence = 'Hello World2'
     para_1.sentence_containing.append(sen_1)
     para_1.sentence_containing.append(sen_2)
